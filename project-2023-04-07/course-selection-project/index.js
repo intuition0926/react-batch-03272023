b584e8cc95e0c7bf98354dc2f3e3888632e1675c
intuition0926 <laofuzi.mit@gmail.com>
2023-04-07 15:59:34 -0400

    add select feature

--- project-2023-04-07/course-selection-project/index.js
+++ project-2023-04-07/course-selection-project/index.js
@@ -24,12 +24,16 @@ class CourseModel {
 
 class CourseView {
     constructor() {
-        this.allCourse = document.querySelector(".all-courses");
-        this.selectedCourse = document.querySelector(".selected-Courses");
+        this.allCourses = document.querySelector(".all-courses");
+        this.selectedCourses = document.querySelector(".selected-Courses");
+        this.selectButton = document.querySelector("button");
+        this.p = document.getElementById("demo");
+        this.totalCredit = document.querySelector(".totalCredit");
     }
 
     createCourse(course) {
         const courseElem = document.createElement("li");
+        courseElem.setAttribute("selected", false);
         courseElem.id = course.id;
         const courseName = document.createElement("div");
         courseName.innerText = course.courseName;
@@ -40,12 +44,46 @@ class CourseView {
         courseElem.appendChild(courseName);
         courseElem.appendChild(courseType);
         courseElem.appendChild(courseCredit);
-        this.allCourse.appendChild(courseElem);
+        this.allCourses.appendChild(courseElem);
+    }
+
+    createSelectedCourse(course) {
+        const courseElem = document.createElement("li");
+        courseElem.setAttribute("selected", false);
+        courseElem.id = course.id;
+        const courseName = document.createElement("div");
+        courseName.innerText = course.courseName;
+        const courseType = document.createElement("div");
+        courseType.innerText = "Course Type : " + (course.required ? "Compulsory" : "Elective");
+        const courseCredit = document.createElement("div");
+        courseCredit.innerText = "Course Credit : " + course.credit;
+        courseElem.appendChild(courseName);
+        courseElem.appendChild(courseType);
+        courseElem.appendChild(courseCredit);
+        this.selectedCourses.appendChild(courseElem);
     }
 
     displayAllCourses(courses) {
         courses.forEach(course => {
             this.createCourse(course);
+        })
+    }
+
+    displaySelectedCourses(courses) {
+        courses.forEach(course => {
+            this.createSelectedCourse(course);
+        })
+    }
+
+    bindHandleSelect() {
+        this.selectButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            var txt = "You have chosen " + this.totalCredit.innerText + " credits for this semester. You cannot change once you submit. Do you want to confirm?";
+            if (confirm(txt)) {
+                
+            } else {
+                
+            }
         })
     }
 }
@@ -60,7 +98,10 @@ class CourseController {
     init() {
         this.model.fetchCourses().then(() => {
             const courses = this.model.allCourses;
+            const selectedCourses = this.model.selectedCourses;
             this.view.displayAllCourses(courses);
+            this.view.displaySelectedCourses(selectedCourses);
+            this.view.bindHandleSelect();
         })
     }
 }
